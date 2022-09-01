e0885a24baca3d0af491464fbbc840cd1edba663
Ankit <96181487+abg5612@users.noreply.github.com>
2022-09-01 16:18:47 +0530

    Updates

---
+++ precache-manifest.9d8422e513e622d26f44abd6273c712f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "357766ac20bda57062e5",
+    "url": "/Textutilsmy-app/static/css/main.b9fd9ed8.chunk.css"
+  },
+  {
+    "revision": "357766ac20bda57062e5",
+    "url": "/Textutilsmy-app/static/js/main.357766ac.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/Textutilsmy-app/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/Textutilsmy-app/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "f746c4d64f532639c72b",
+    "url": "/Textutilsmy-app/static/js/runtime~main.f746c4d6.js"
+  },
+  {
+    "revision": "2d1f8cdc04d9b7abe7fc527bf5265632",
+    "url": "/Textutilsmy-app/index.html"
+  }
+];
